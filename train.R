61229d4edfe52174378b189a7668b3df2d4e054c
Leon Hafner <leon.hafner@tum.de>
2024-07-02 13:59:40 +0200

    Implemented new train approach

--- train.R
+++ train.R
@@ -1,5 +1,20 @@
 library(naturalsort)
 
+load_data <- function(input_path) {
+  train.paths <- naturalsort(list.files(input_path, pattern = "train_*"))
+  test.paths <- naturalsort(list.files(input_path, pattern = "test_*"))
+  
+  train <- vector("list", length(train.paths))
+  test <- vector("list", length(test.paths))
+  
+  for (i in 1:length(train.paths)) {
+    train[[i]] <- fread(file.path(input_path, train.paths[i]))
+    test[[i]] <- fread(file.path(input_path, test.paths[i]))
+  }
+  return(list(train = train, test = test))
+}
+
+
 hfsp <- function(params, alnlen, pident) {
   factor <- params[1]
   exponent <- params[2]
@@ -14,6 +29,7 @@ hfsp <- function(params, alnlen, pident) {
   return(pident - hfsp_val)
 }
 
+
 f1_score <- function(y_true, y_pred) {
   confusion_matrix <- table(factor(y_true, levels = c(0, 1)), factor(y_pred, levels = c(0, 1)))
   TP <- confusion_matrix[2, 2]
@@ -25,63 +41,52 @@ f1_score <- function(y_true, y_pred) {
   return(f1)
 }
 
-objective_function <- function(params) {
-  f1_scores <- c()
-  
-  for (i in 1:length(train)) {
-    train_data <- train[[i]]
-    test_data <- test[[i]]
-    
-    # Apply the HFSP function to the test data
-    test_data$hfsp <- mapply(hfsp, MoreArgs = list(params = params), test_data$ug_alnlen, test_data$pident)
-    y_true <- test_data$y_true
-    y_pred <- ifelse(test_data$hfsp > 0, 1, 0)
-    
-    f1 <- f1_score(y_true, y_pred)
-    f1_scores <- c(f1_scores, f1)
-  }
-  
-  average_f1 <- mean(f1_scores)
-  return(-average_f1)
-}
 
-input.path <- "/nfs/home/students/l.hafner/pp1/Enhance_HFSP/testing/4_after_cv_split/with_undersampling"
-
-train.paths <- naturalsort(list.files(input.path, pattern = "train_*"))
-test.paths <- naturalsort(list.files(input.path, pattern = "test_*"))
-
-train <- vector("list", length(train.paths))
-test <- vector("list", length(test.paths))
-
-for (i in 1:length(train.paths)) {
-  train[[i]] <- fread(file.path(input.path, train.paths[i]))
-  test[[i]] <- fread(file.path(input.path, test.paths[i]))
+objective_function <- function(params, train_data) {
+  train_data$hfsp <- mapply(hfsp, MoreArgs = list(params = params), train_data$ug_alnlen, train_data$pident)
+  y_true <- train_data$y_true
+  y_pred <- ifelse(train_data$hfsp > 0, 1, 0)
+  return(-f1_score(y_true, y_pred))
 }
 
 
+data <- load_data("/nfs/home/students/l.hafner/pp1/Enhance_HFSP/testing/4_after_cv_split/with_undersampling")
+
+train <- data$train
+test <- data$test
+
+results <- data.table(fold = integer(), factor = numeric(), exponent = numeric(), f1_score = numeric())
 
 factors <- seq(300, 1500, by = 50)
 exponents <- seq(-0.3, -0.9, by = -0.05)
-
-results <- data.table(factor = numeric(), exponent = numeric(), f1_score = numeric())
 
 for (factor in factors) {
   for (exponent in exponents) {
     print(paste("Testing factor", factor, "and exponent", exponent))
     initial_params <- c(factor, exponent)
-    result <- optim(initial_params, objective_function, method = "Nelder-Mead")
-    best_factor <- result$par[1]
-    best_exponent <- result$par[2]
-    best_f1 <- -result$value
-    results <- rbind(results, data.table(factor = best_factor, exponent = best_exponent, f1_score = best_f1))
+    
+    for (i in 1:length(train)) {
+      if (i > 1) {
+        break
+      }
+      print(paste("Fold", i))
+      train_data <- train[[i]]
+      test_data <- test[[i]]
+      
+      optim_result <- optim(initial_params, objective_function, train_data = train_data, method = "Nelder-Mead")
+      best_params <- optim_result$par
+      
+      test_data$hfsp <- mapply(hfsp, MoreArgs = list(params = best_params), test_data$ug_alnlen, test_data$pident)
+      y_true <- test_data$y_true
+      y_pred <- ifelse(test_data$hfsp > 0, 1, 0)
+      
+      f1 <- f1_score(y_true, y_pred)
+      results <- rbind(results, data.table(fold = i, factor = best_params[1], exponent= best_params[2], f1_score = f1))
+    }
   }
 }
 
-best_result <- results[which.max(f1_score)]
-print(best_result)
+fwrite(results, "/nfs/home/students/l.hafner/pp1/Enhance_HFSP/results_run1.tsv", sep ="\t")
 
-
-
-
-
-
+#summary_results <- results[, .(mean_f1_score = mean(f1_score)), by = .(factor, exponent)]
+#summary_results[mean_f1_score == max(mean_f1_score)]
